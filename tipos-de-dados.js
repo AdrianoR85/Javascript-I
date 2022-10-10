b73b75406517ec70c57ec88df15230dd2e56a68a
adrianor85 <adrianorosa085@gmail.com>
2022-10-10 15:49:24 -0300

    tipos de dados

---
+++ tipos-de-dados.js
@@ -0,0 +1,20 @@
+// number
+console.log(5 + 5)
+// bigint
+console.log(3n +3n)
+
+// string
+console.log('Adriano' + 'R' + '85')
+console.log('Adriano'[2]) // mostra a terceira letra do nome. 
+console.log('Adriano'.length) // mostra quantas letras tem o nome.
+
+//Boolean
+console.log(2 < 1);
+
+// undefined
+let name;
+console.log(name)
+
+// null
+let num =null;
+console.log(num)
