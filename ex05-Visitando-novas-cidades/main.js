699698d7b1f38cda13eff76c96fa22171acc78bc
adrianor85 <adrianorosa085@gmail.com>
2022-10-13 15:34:23 -0300

    Visitting cities

---
+++ ex05-Visitando-novas-cidades/main.js
@@ -0,0 +1,19 @@
+const name = prompt('Informe o seu nome:')
+
+let moreCity = prompt('Você já vistou alguma cidade').toLowerCase()
+let cities = ""
+let totalCity = 0
+
+while(moreCity.trim() === "sim") {
+    const city = prompt('Qual cidade: ')
+    cities += ` - ${city}\n`
+    totalCity ++
+
+    moreCity = prompt('Você vistou mais alguma cidade: ')
+}
+
+alert(`
+    Turista: ${name}
+    Total de cidades visitadas: ${totalCity}
+    cidades:\n ${cities}
+`)
