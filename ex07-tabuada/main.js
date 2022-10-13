6882a318fc94cc871b4ac8e2efd676bf27be7d28
adrianor85 <adrianorosa085@gmail.com>
2022-10-13 16:11:48 -0300

    for

--- ex07-tabuada/main.js
+++ ex07-tabuada/main.js
@@ -1,11 +1,10 @@
 const number = Number(prompt('Robo da Tabuada\n Você deseja ver a tabuada de qual número? '))
 let multiplicationTable = ""
-let index = 1
-do {
+
+for(let index = 1; index < 21; index++){
     result = number * index
     multiplicationTable += `${number} x ${index} = ${result}\n`
-    index ++
-} while (index < 21)
+} 
 
 alert(
     "Resultado da tabuada do " + number + "\n" + multiplicationTable
