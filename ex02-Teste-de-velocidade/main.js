59656a47d24a1ad5bfb41a52b0b430a44068454b
adrianor85 <adrianorosa085@gmail.com>
2022-10-13 15:39:51 -0300

    if and else

--- ex02-Teste-de-velocidade/main.js
+++ ex02-Teste-de-velocidade/main.js
@@ -5,9 +5,9 @@ const carSpeed02 = prompt('Velocidade do carro 02: ')
 
 
 if(carSpeed01 > carSpeed02) {
-    alert(`O ${carName01} é mais rápido`)
+    alert(`O ${carName01} é mais rápido que o ${carName02}`)
 } else if(carSpeed01 < carSpeed02) {
-    alert(`O ${carName02} é mais rápido`)
+    alert(`O ${carName02} é mais rápido que o ${carName02}`)
 } else {
     alert('Os dois carro estão na mesma velocidade.')
 }
