68434b12492295a808b7a026c205478fd474d9cb
adrianor85 <adrianorosa085@gmail.com>
2022-10-13 15:38:14 -0300

    prompt

--- ex01-Cadastro-de-Recrutas/main.js
+++ ex01-Cadastro-de-Recrutas/main.js
@@ -2,10 +2,8 @@ const fname = prompt('Informe o seu primeiro nome: ');
 const lastname = prompt('Informe o seu segundo nome: ')
 const study = prompt('O que você está estudando? ')
 const birthday = prompt('Qual o seu ano de nascimento? ')
-const data = new Date();
-const currentYear = data.getFullYear();
 
-const age = currentYear - birthday;
+const age = 2022 - birthday;
 console.log(age)
 
 alert(` Novo recruta:
