8efdc5b677d6639fe06b025b69231658512824b1
adrianor85 <adrianorosa085@gmail.com>
2022-10-18 16:37:20 -0300

    Objects

---
+++ ex12-Cadastro-de-Imoveis/main.js
@@ -0,0 +1,52 @@
+let properties = []
+let option = ""
+
+do {
+    const totalproperties = properties.length;
+    option = prompt(
+    "Imóveis Cadastrado: " + totalproperties + "\n"
+     + "\nEscolha uma Opção: "
+     + "\n1 - Cadastrar no Imóveil"
+     + "\n2 - Listar Imóveis"
+     + "\n3 - Sair"
+    )
+
+    switch(option) {
+        case "1":
+            const propertie = {}
+            propertie.name = prompt("Nome do Proprietário: ")
+            propertie.room = prompt('Quantidade de quartos: ')
+            propertie.bath = prompt("Quantidade de banheiros: ")
+            propertie.garage = prompt("Possui garagem? ")
+
+            const save = confirm(
+                "Deseja salvar este imóvel?\n" +
+                "\nProprietário: " + propertie.name +
+                "\nQuartos: " + propertie.room +
+                "\nBanheiros: " + propertie.bath +
+                "\nPossui garagem? " + propertie.garage
+            )
+
+            if (save) {
+                properties.push(propertie)
+            }
+            break
+        case "2":
+            let showProperties = ""
+            for(let i = 0; i < properties.length; i++) {
+                showProperties +=
+                "\nProprietário: " + properties[i].name +
+                "\nQuartos: " + properties[i].room +
+                "\nBanheiros: " + properties[i].bath +
+                "\nPossui garagem? " + properties[i].garage + "\n"
+            }
+            alert("Propriedades:\n" + showProperties)
+            break
+        case "3":
+            alert('Saindo...')
+            break
+        default:
+            alert("Opção inválida")
+            break
+    }
+} while(option !== "3")
